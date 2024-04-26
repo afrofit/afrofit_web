83783231541c4a254f282a7d92085303b0a1d861
dipenshivlab <dipen@shivlab.com>
2024-04-26 15:18:23 +0530

    - add user logs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,50 +1,51 @@
-import * as React from 'react'
-import '@stripe/stripe-js'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { useDispatch, useSelector } from 'react-redux'
-import { ErrorDialog } from '../components/dialogs/ErrorDialog'
-import { FullPageLoadingSpinner } from '../components/elements/FullPageLoadingSpinner'
-import { theme } from '../constants/theme'
+import * as React from 'react';
+import '@stripe/stripe-js';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { ErrorDialog } from '../components/dialogs/ErrorDialog';
+import { FullPageLoadingSpinner } from '../components/elements/FullPageLoadingSpinner';
+import { theme } from '../constants/theme';
 import {
   hideGenericErrorDialog,
   hideGenericSuccessDialog,
   selectedShowSuccessMessage,
   selectShowGenericErrorDialog,
   selectUiIsLoading,
-} from '../store/reducers/ui/ui.slice'
-import { AppRouter } from './AppRouter'
+} from '../store/reducers/ui/ui.slice';
+import { AppRouter } from './AppRouter';
 import {
   selectUserToken,
   storeUser,
   storeUserToken,
-} from '../store/reducers/auth/auth.slice'
-import { DECODE_TOKEN, GET_TOKEN } from '../api/storage'
-import { SuccessDialog } from '../components/dialogs/SuccessDialog'
+} from '../store/reducers/auth/auth.slice';
+import { DECODE_TOKEN, GET_TOKEN } from '../api/storage';
+import { SuccessDialog } from '../components/dialogs/SuccessDialog';
 
 const App: React.FC = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const isLoading = useSelector(selectUiIsLoading)
-  const errorMessage = useSelector(selectShowGenericErrorDialog)
-  const successMessage = useSelector(selectedShowSuccessMessage)
+  const isLoading = useSelector(selectUiIsLoading);
+  const errorMessage = useSelector(selectShowGenericErrorDialog);
+  const successMessage = useSelector(selectedShowSuccessMessage);
 
-  const userToken = useSelector(selectUserToken)
-
-  const memoToken = React.useMemo(() => GET_TOKEN(), [])
-
+  const userToken = useSelector(selectUserToken);
+  console.log('userToken :>> ', userToken);
+  const memoToken = React.useMemo(() => GET_TOKEN(), []);
+  console.log('memoToken :>> ', memoToken);
   const putTokenInStore = React.useCallback(() => {
-    memoToken && dispatch(storeUserToken(memoToken))
-  }, [memoToken, dispatch])
+    memoToken && dispatch(storeUserToken(memoToken));
+  }, [memoToken, dispatch]);
 
   React.useEffect(() => {
-    putTokenInStore()
-    const user = DECODE_TOKEN()
+    putTokenInStore();
+    const user = DECODE_TOKEN();
+    console.log('user :>> ', user);
     if (user) {
-      dispatch(storeUser(user))
+      dispatch(storeUser(user));
     } else {
-      dispatch(storeUser(undefined))
+      dispatch(storeUser(undefined));
     }
-  }, [dispatch, userToken, putTokenInStore])
+  }, [dispatch, userToken, putTokenInStore]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -66,7 +67,7 @@ const App: React.FC = () => {
       <FullPageLoadingSpinner isLoading={isLoading} />
       <AppRouter />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
